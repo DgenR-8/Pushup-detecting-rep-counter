ea588ba88256e30a6069ee4cd9a5ce8db4984513
DgenR-8 <jdstabios200094@gmail.com>
2024-01-05 14:41:24 +0000

    Complete kind of

--- MotionDetectingRepCounter.py
+++ MotionDetectingRepCounter.py
@@ -14,20 +14,20 @@ initial_frame = imutils.resize(initial_frame, width=500)
 initial_frame = cv2.cvtColor(initial_frame, cv2.COLOR_BGR2GRAY)
 initial_frame = cv2.GaussianBlur(initial_frame, (21, 21), 0)
 
-alarm = False
-alarm_activation = False
+rep = False
+rep_activation = False
 alarm_counter = 0
-rep_counter = 0
 
-def beep_alarm():
-    global alarm
-    for _ in range(1, 100): # number of repeats of alarm before breaking
-        if not alarm_activation:
+def beep_rep():
+    
+    global rep
+    for _ in range(1): # number of repeats of alarm rep before breaking
+        if not rep_activation:
             break
-        rep_counter = _+1
-        print("You are on rep ", rep_counter)
-        winsound.Beep(3000, 500) # Frequency of sound, duration of sound in milliseconds
-    alarm = False
+        print("You have completed one rep")
+        winsound.Beep(400, 500) # Frequency of sound, duration of sound in milliseconds
+        
+    rep = False
 
 
 while True:
@@ -35,35 +35,35 @@ while True:
     _, frame = cap.read() 
     frame = imutils.resize(frame, width=500)
 
-    if alarm_activation:
+    if rep_activation:
         frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
 
         difference = cv2.absdiff(frame_bw, initial_frame)
-        threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+        threshold = cv2.threshold(difference, 96, 1977, cv2.THRESH_BINARY)[1]
         initial_frame = frame_bw
 
-        if threshold.sum() > 1000: # Sensitivity of motion detection
-            alarm_counter += 1 # If sensitivity is triggered alarm counter accumulates
+        if threshold.sum() > 1000000: # Sensitivity of motion detection
+            alarm_counter += 1 # If sensitivity is triggered rep counter accumulates
         else:
-            if alarm_counter > 0: # If senstivity isn't trigger (e.g. no motion detected)
-                alarm_counter -= 1 # Alarm counter decreases, subsequently reducing alarm activations
+            if alarm_counter > 0: # If senstivity isn't triggered (e.g. no motion detected)
+                alarm_counter -= 1 # Rep counter decreases, subsequently reducing alarm activations
         
         cv2.imshow("Cam", threshold)
     else:
         cv2.imshow("Cam", frame)
     
     if alarm_counter > 1: # the higher the counter the higher the frequency of alarm activations
-        if not alarm:
-            alarm = True
-            threading.Thread(target=beep_alarm).start()
+        if not rep:
+            rep = True
+            threading.Thread(target=beep_rep).start()
 
     key_pressed = cv2.waitKey(15) # Keypress delay
     if key_pressed == ord("t"):
-        alarm_activation = not alarm_activation
+        rep_activation = not rep_activation
         alarm_counter = 0
     if key_pressed == ord("q"):
-        alarm_activation = False
+        rep_activation = False
         break
 
 
